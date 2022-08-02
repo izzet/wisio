ea96e10481b6b2171fa57679d76f801bce74b664
Izzet Yildirim <yildirim2@llnl.gov>
2022-08-02 15:53:52 -0700

    Cluster shutdown functionality added.

--- vani/__main__.py
+++ vani/__main__.py
@@ -1,8 +1,5 @@
-
 from vani.analyzer import Analyzer, ClusterOptions, ClusterType
 
-from vani.common.filter_groups import TimelineFilterGroup
-from vani.common.filters import IOSizeFilter
 
 if __name__ == '__main__':
 
@@ -17,11 +14,16 @@ if __name__ == '__main__':
     # log_dir = "/p/gpfs1/iopp/recorder_app_logs/montage_pegasus/nodes-32/_parquet"
     # log_dir = "/p/gpfs1/iopp/recorder_app_logs/genome_pegasus/nodes-32/_parquet"
 
-    log_dir = "/p/lustre1/iopp/parquet_app_logs/hacc/nodes-32/workflow-0"
-    # log_dir = "/p/lustre1/iopp/parquet_app_logs/cm1/nodes-32/workflow-4"
+    # log_dir = "/p/lustre1/iopp/parquet_app_logs/hacc/nodes-32/workflow-0"
+    log_dir = "/p/lustre1/iopp/parquet_app_logs/cm1/nodes-32/workflow-4"
     # log_dir = "/p/lustre1/iopp/parquet_app_logs/lbann-cosmoflow/nodes-32"
     # log_dir = "/p/lustre1/iopp/recorder_app_logs/montage_pegasus/nodes-32/_parquet"
     # log_dir = "/p/lustre1/iopp/recorder_app_logs/genome_pegasus/nodes-32/_parquet"
 
-    # Do the analysis
-    analysis = vn.analyze_parquet_logs(log_dir)
+    try:
+        # Do the analysis
+        analysis = vn.analyze_parquet_logs(log_dir, max_depth=5)
+        analysis.render_tree()
+
+    finally:
+        vn.shutdown()
