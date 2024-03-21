04a6bf2634527347306fe2f658aae1e28ee72aac
Izzet Yildirim <yildirim2@llnl.gov>
2024-03-21 16:32:29 -0700

    Config helper improvements.

--- config_samples/config_helper.py
+++ config_samples/config_helper.py
@@ -2,6 +2,33 @@ import argparse
 import yaml
 
 
+def delete(my_dict, key_string):
+    here = my_dict
+    keys = key_string.split(".")
+    for key in keys[:-1]:
+        here = here.setdefault(key, {})
+    try:
+        del here[keys[-1]]
+    except KeyError:
+        pass
+
+
+def get(my_dict, key_string):
+    here = my_dict
+    keys = key_string.split(".")
+    for key in keys[:-1]:
+        here = here.setdefault(key, {})
+    return here[keys[-1]] if keys[-1] in here else ''
+
+
+def set(my_dict, key_string, value):
+    here = my_dict
+    keys = key_string.split(".")
+    for key in keys[:-1]:
+        here = here.setdefault(key, {})
+    here[keys[-1]] = set_value(value)
+
+
 def set_value(value):
     if value.isdigit():
         return int(value)
@@ -15,22 +42,6 @@ def set_value(value):
         return value
 
 
-def get(my_dict, key_string):
-    here = my_dict
-    keys = key_string.split(".")
-    for key in keys[:-1]:
-        here = here.setdefault(key, {})
-    return here[keys[-1]]
-
-
-def set(my_dict, key_string, value):
-    here = my_dict
-    keys = key_string.split(".")
-    for key in keys[:-1]:
-        here = here.setdefault(key, {})
-    here[keys[-1]] = set_value(value)
-
-
 def main():
 
     parser = argparse.ArgumentParser()
@@ -41,6 +52,7 @@ def main():
 
     subparsers = parser.add_subparsers(title='commands', dest='command')
 
+    del_parser = subparsers.add_parser('del', parents=[base_parser])
     get_parser = subparsers.add_parser('get', parents=[base_parser])
     set_parser = subparsers.add_parser('set', parents=[base_parser])
     set_parser.add_argument('value')
@@ -50,11 +62,20 @@ def main():
     with open(args.path, 'r') as file:
         config = yaml.safe_load(file)
 
-    if args.command == 'get':
+    modified = False
+
+    if args.command == 'del':
+        print('Deleting', args.key, 'from', args.path)
+        delete(config, args.key)
+        modified = True
+    elif args.command == 'get':
         print(get(config, args.key))
     elif args.command == 'set':
         print('Setting', args.key, 'to', args.value, 'for', args.path)
         set(config, args.key, args.value)
+        modified = True
+
+    if modified:
         with open(args.path, 'w') as file:
             yaml.safe_dump(config, file)
 
