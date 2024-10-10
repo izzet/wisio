5d8dc89d2ef2cc11c0f3e05245a985391b3247d6
Izzet Yildirim <yildirim2@llnl.gov>
2024-10-10 00:47:05 -0700

    Add analysis notebooks.

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -1,53 +1,83 @@
 import papermill as pm
 
 checkpoint_dir = "/usr/workspace/iopp/wisio_logs/_checkpoints"
-dftracer_trace_dir = "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona"
 
 traces = [
     # (
     #     "dftracer",
     #     "bert-v100-node-4-v1",
-    #     f"{dftracer_trace_dir}/bert/v100/node-4/v1/RAW/*.pfw.gz",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/bert/v100/node-4/v1/COMPACT/*.pfw.gz",
     # ),
     # (
     #     "dftracer",
-    #     "bert-v100-node-16-v2",
-    #     f"{dftracer_trace_dir}/bert/v100/node-16/v2/RAW/*.pfw.gz",
+    #     "bert-v100-node-16-v3",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/bert/v100/node-16/v3/COMPACT/*.pfw.gz",
     # ),
     # (
     #     "dftracer",
-    #     "cm1-n1-ppn48",
-    #     f"{dftracer_trace_dir}/cm1_1_48_20240926/cm1_1_48_20240926-COMPACT/*.pfw.gz",
+    #     "cm1-node-1",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/cm1/APP/node-1/v1/COMPACT/*.pfw.gz",
     # ),
     # (
     #     "dftracer",
-    #     "cm1-n32-ppn32",
-    #     f"{dftracer_trace_dir}/cm1_32_32_20240926/cm1_32_32_20240926-COMPACT/*.pfw.gz",
+    #     "cm1-node-32",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/cm1/APP/node-32/v1/COMPACT/*.pfw.gz",
     # ),
     # (
     #     "dftracer",
     #     "cosmoflow-dlio-v100-node-4-v1",
-    #     f"{dftracer_trace_dir}/cosmoflow/dlio-v100/node-4/v1/RAW/*.pfw.gz",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/cosmoflow/dlio-v100/node-4/v1/COMPACT/*.pfw.gz",
+    # ),
+    # (
+    #     "dftracer",
+    #     "cosmoflow-dlio-v100-node-16-v3",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/cosmoflow/dlio-v100/node-16/v3/COMPACT/*.pfw.gz",
+    # ),
+    # (
+    #     "dftracer",
+    #     "montage-pegasus-dss-2deg-node-4",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/montage/pegasus-dss-2deg/node-4/v1/COMPACT/*.pfw.gz",
+    # ),
+    # (
+    #     "dftracer",
+    #     "montage-pegasus-dss-2deg-node-16",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/montage/pegasus-dss-2deg/node-16/v1/COMPACT/*.pfw.gz",
     # ),
     # (
     #     "dftracer",
     #     "resnet50-dlio-v100-node-4-v1",
-    #     f"{dftracer_trace_dir}/resnet50/dlio-v100/node-4/v1/RAW/*.pfw.gz",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/resnet50/dlio-v100/node-4/v1/COMPACT/*.pfw.gz",
     # ),
     # (
     #     "dftracer",
-    #     "resnet50-dlio-v100-node-16-v2",
-    #     f"{dftracer_trace_dir}/resnet50/dlio-v100/node-16/v2/RAW/*.pfw.gz",
+    #     "resnet50-dlio-v100-node-16-v3",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/resnet50/dlio-v100/node-16/v3/COMPACT/*.pfw.gz",
+    # ),
+    # /p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/unet3d/dlio-v100/node-1/v1/RAW
+    # (
+    #     "dftracer",
+    #     "unet3d-dlio-v100-node-1-v1",
+    #     "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/unet3d/dlio-v100/node-1/v1/RAW/*.pfw.gz",
     # ),
     (
         "dftracer",
-        "deepspeed-dlio-node-16",
-        f"{dftracer_trace_dir.replace('traces-lfs', 'traces')}/megatron_deepspeed-1_16_8/megatron_deepspeed-1_16_8-COMPACT/*.pfw.gz",
+        "unet3d-dlio-v100-node-16-v2",
+        "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/unet3d/dlio-v100/node-16/v2/RAW/*.pfw.gz",
     ),
+    # (
+    #     "dftracer",
+    #     "deepspeed-dlio-node-16",
+    #     f"{dftracer_trace_dir.replace('traces-lfs', 'traces')}/megatron_deepspeed-1_16_8/megatron_deepspeed-1_16_8-COMPACT/*.pfw.gz",
+    # ),
+    # (
+    #     "dftracer",
+    #     "deepspeed-dlio-scr-node-16",
+    #     f"{dftracer_trace_dir.replace('traces-lfs', 'traces')}/megatron_deepspeed-1-100-1_16_8_scr/megatron_deepspeed-1-100-1_16_8_scr-COMPACT/*.pfw.gz",
+    # ),
     (
         "dftracer",
-        "deepspeed-dlio-scr-node-16",
-        f"{dftracer_trace_dir.replace('traces-lfs', 'traces')}/megatron_deepspeed-1-100-1_16_8_scr/megatron_deepspeed-1-100-1_16_8_scr-COMPACT/*.pfw.gz",
+        "1000-genome-pegasus-node-32",
+        "/p/lustre3/iopp/dftracer-traces-lfs/v1.0.5-develop/corona/1000-genome/pegasus/node-32/v1/COMPACT/*.pfw.gz",
     ),
 ]
 
@@ -63,6 +93,13 @@ for analyzer, trace_name, trace_path in traces:
             output_max_bottlenecks=1,
             output_root_only=False,
             run_dir=f".wisio/{analyzer}/{trace_name}",
+            percentile=0.99,
             trace_path=trace_path,
         ),
     )
+
+# bert (4 node - 8 GPUs per node = 32 processes)
+# 1000 steps
+# every step reads 48 images/samples of 2500 bytes each
+# https://github.com/hariharan-devarajan/iopp/blob/system/corona/apps/dlio/script.sh
+# do `prefetch_size` of 4GB
