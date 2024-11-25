1f10806ed7e1eee5afd40399e021b02bf37b6f8b
Izzet Yildirim <iyildirim@hawk.iit.edu>
2024-11-25 18:33:07 +0000

    Refactor metrics for improved clarity and consistency.

--- wisio/metrics.py
+++ wisio/metrics.py
@@ -58,7 +58,6 @@ class Metric(abc.ABC):
         self.name = name
         self.is_normalized = is_normalized
         self.is_reversed = is_reversed
-        self.score_col = f"{name}_score"
 
     @abc.abstractmethod
     def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
@@ -69,28 +68,21 @@ class Metric(abc.ABC):
         raise NotImplementedError
 
     def score_slope(self, df: pd.DataFrame, value_col: str) -> pd.DataFrame:
-        return self._score(
-            df=df,
-            value_col=value_col,
-            bins=SLOPE_BINS,
-            names=SCORE_NAMES,
-        )
+        return self._score(df, value_col, SLOPE_BINS, SCORE_NAMES, True)
 
     def score_percentage(self, df: pd.DataFrame, value_col: str) -> pd.DataFrame:
-        return self._score(
-            df=df,
-            value_col=value_col,
-            bins=PERCENTAGE_BINS,
-            names=SCORE_NAMES,
-        )
+        return self._score(df, value_col, PERCENTAGE_BINS, SCORE_NAMES, False)
 
     def score_percentile(self, df: pd.DataFrame, value_col: str) -> pd.DataFrame:
         return self.score_percentage(df, value_col)
 
-    def query_column_name(self, slope=False) -> str:
+    def metric_column(self, slope=False) -> str:
         if slope:
             return f"{self.name}_slope"
-        return f"{self.name}_percentile"
+        return f"{self.name}_per"
+
+    def score_column(self, slope=False) -> str:
+        return f"{self.metric_column(slope)}_score"
 
     def __str__(self):
         return self.name
@@ -101,48 +93,76 @@ class Metric(abc.ABC):
         value_col: str,
         bins: List[Any],
         names: List[str],
+        slope: bool = False,
     ) -> pd.DataFrame:
-        bin_col = f"{self.name}_bin"
+        bin_col = f"{self.metric_column(slope)}_bin"
+        score_col = self.score_column(slope)
         df[bin_col] = np.digitize(df[value_col], bins=bins, right=True)
-        df[self.score_col] = np.choose(df[bin_col] - 1, choices=names, mode='clip')
+        df[score_col] = np.choose(df[bin_col] - 1, choices=names, mode='clip')
         return df.drop(columns=[bin_col])
 
 
-class BandwidthMetric(Metric):
-    def __init__(self, name: str):
-        super().__init__(name, is_reversed=True)
-        self.score_col = f"{self.name}_bw_score"
+class NormalizedMetric(Metric):
+    def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        df[self.metric_column()] = df[self.name] / boundary
+        return df
+
+    def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        return self.score_percentage(df, self.metric_column())
+
+    def metric_column(self, slope=False) -> str:
+        return f"{self.name}_norm"
+
+
+class PercentageMetric(Metric):
+    def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        df[self.metric_column()] = df[self.name] / df[self.name].sum()
+        return df
+
+    def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        return self.score_percentage(df, self.metric_column())
+
+    def metric_column(self, slope=False) -> str:
+        return f"{self.name}_per"
+
+
+class RatioMetric(Metric):
+    def __init__(self, name: str, val_col: str, div_col: str):
+        super().__init__(name)
+        self.val_col = val_col
+        self.div_col = div_col
 
     def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        df[f"{self.name}_bw"] = df[f"{self.name}_size"] / df[f"{self.name}_time"]
+        df[self.name] = 0.0
+        df[self.name] = df[self.name].mask(
+            df[self.div_col] > 0,
+            df[self.val_col] / df[self.div_col],
+        )
+        return df
+
+    def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        return self.score_percentage(df, self.name)
+
+    def metric_column(self, slope=False) -> str:
+        return self.name
+
+
+class BandwidthMetric(Metric):
+    def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        df[self.metric_column()] = df[f"{self.name}_size"] / df[f"{self.name}_time"]
         return df
 
     def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
         bins = BW_BINS_PER_PROC if COL_PROC_NAME in df.index.names else BW_BINS
         return self._score(
             df=df,
-            value_col=f"{self.name}_bw",
+            value_col=self.metric_column(),
             bins=bins,
             names=np.flip(SCORE_NAMES),
         )
 
-    def query_column_name(self, slope=False) -> str:
+    def metric_column(self, slope=False) -> str:
         return f"{self.name}_bw"
-
-
-class TimeMetric(Metric):
-    def __init__(self):
-        super().__init__("time")
-
-    def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        df["time_normalized"] = df["time"] / boundary
-        return df
-
-    def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        return self.score_percentage(df, "time_normalized")
-
-    def query_column_name(self, slope=False) -> str:
-        return "time_normalized"
 
 
 class OpsMetric(Metric):
@@ -155,36 +175,24 @@ class OpsMetric(Metric):
         df["ops_slope"] = df["count_per"] / df["time_per"]
         drop_cols = ["time_per", "count_per"]
         if not slope:
-            df["ops_percentile"] = (1 / df["ops_slope"]).rank(pct=True)
+            df["ops_rank"] = (1 / df["ops_slope"]).rank(pct=True)
             drop_cols.append("ops_slope")
         return df.drop(columns=drop_cols, errors="ignore")
 
     def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
+        value_col = self.metric_column(slope)
         if slope:
-            return self.score_slope(df, "ops_slope")
-        return self.score_percentile(df, "ops_percentile")
+            return self.score_slope(df, value_col)
+        return self.score_percentile(df, value_col)
+
+    def metric_column(self, slope=False) -> str:
+        if slope:
+            return "ops_slope"
+        return "ops_rank"
 
 
-class IOComputeRatioMetric(Metric):
-    def __init__(self):
-        super().__init__("io_compute_ratio")
-
-    def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        df["io_compute_ratio"] = 0.0
-        df["io_compute_ratio"] = df["io_compute_ratio"].mask(
-            df["compute_time"] > 0, df["io_time"] / df["compute_time"]
-        )
-        return df
-
-    def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        return self.score_percentage(df, "io_compute_ratio")
-
-    def query_column_name(self, slope=False) -> str:
-        return "io_compute_ratio"
-
-
-class UnoverlappedDerivedIOMetric(Metric):
-    def __init__(self, name: str):
+class DerivedRatioMetric(Metric):
+    def __init__(self, name: str, val_col: str, div_col: str, derived_col: str):
         super().__init__(name)
         self.is_normalized = True
         self.score_col = f"u_{name}_score"
@@ -206,52 +214,43 @@ class UnoverlappedDerivedIOMetric(Metric):
     def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
         return self.score_percentage(df, f"u_{self.name}_per")
 
-    def query_column_name(self, slope=False) -> str:
+    def metric_column(self, slope=False) -> str:
         return f"u_{self.name}_per"
 
 
-class UnoverlappedIOMetric(Metric):
-    def __init__(self, name: str = "u_io_time"):
-        super().__init__(name)
-        self.is_normalized = True
-
-    def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        df[f"{self.name}_per"] = df[self.name] / df['time']
-        return df
-
-    def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        return self.score_percentage(df, f"{self.name}_per")
-
-    def query_column_name(self, slope=False) -> str:
-        return f"{self.name}_per"
-
-
 class IntensityMetric(Metric):
-    def __init__(self, name: str):
-        super().__init__(f"{name}")
+    def __init__(self):
+        super().__init__("intensity")
 
     def calculate(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
         df['intensity'] = 0.0
         df['intensity'] = df['intensity'].mask(df['size'] > 0, df['count'] / df['size'])
+        df['intensity_rank'] = df['intensity'].rank(pct=True)
         return df
 
     def score(self, df: pd.DataFrame, boundary: float, slope=False) -> pd.DataFrame:
-        return self.score_percentage(df, f"{self.name}_per")
+        return self.score_percentile(df, self.metric_column())
 
-    def query_column_name(self, slope=False) -> str:
-        return f"{self.name}_per"
+    def metric_column(self, slope=False) -> str:
+        return "intensity_rank"
 
 
 KNOWN_METRICS: Dict[str, Metric] = {
     'data_bw': BandwidthMetric('data'),
-    'io_compute_ratio': IOComputeRatioMetric(),
-    'io_time': TimeMetric(),
+    'intensity': IntensityMetric(),
+    'io_bw': BandwidthMetric('data'),
+    'io_compute_per': RatioMetric('io_compute_per', 'io_time', 'compute_time'),
+    'io_time_norm': NormalizedMetric('io_time'),
+    'io_time_per': PercentageMetric('io_time'),
     'iops': OpsMetric(),
     'ops': OpsMetric(),
     'read_bw': BandwidthMetric('read'),
-    'time': TimeMetric(),
-    'u_checkpoint_io_time': UnoverlappedDerivedIOMetric('checkpoint_io_time'),
-    'u_io_time': UnoverlappedIOMetric(),
-    'u_read_io_time': UnoverlappedDerivedIOMetric('read_io_time'),
+    'time_norm': NormalizedMetric('time'),
+    'time_per': PercentageMetric('time'),
+    # 'u_checkpoint_io_time_per': DerivedRatioMetric(
+    #     'u_checkpoint_io_time_per', 'checkpoint_io_time', 'time', 'compute_time'
+    # ),
+    'u_io_time_per': RatioMetric('u_io_time_per', 'u_io_time', 'time'),
+    # 'u_read_io_time': DerivedRatioMetric('read_io_time'),
     'write_bw': BandwidthMetric('write'),
 }
